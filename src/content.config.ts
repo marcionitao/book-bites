b5a879358b57231625806d13c325d0d710306659
Marcio Nitao <marcionitao@gmail.com>
2026-04-17 13:46:10 +0200

    Add .md file with Book List content

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,12 @@
-import { file } from "astro/loaders";
+import { file, glob } from "astro/loaders";
 import { defineCollection } from "astro:content";
 import { z } from "astro/zod";
 
 const books = defineCollection({
-  loader: file("src/content/books.json"),
+  // loader: file("src/content/books.json"), // -> no caso de ser apena um unico ficheiro .json
+  loader: glob({ base: "src/content/books", pattern: "*.md" }),
   schema: z.object({
-    id: z.string(),
+    // id: z.string(), // -> no caso de ser apena um unico ficheiro .json
     title: z.string(),
     author: z.string(),
     rating: z.number(),
